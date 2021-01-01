f08946f69b0c2a71d6deac50bf2773ce5289f0ca
pankokko <teshi41@gmail.com>
2021-01-01 20:11:05 +0900

    Linebotオウム返し機能

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module go-study
 go 1.15
 
 require (
+	github.com/aws/aws-lambda-go v1.22.0
 	github.com/cosmtrek/air v1.21.2 // indirect
 	github.com/creack/pty v1.1.11 // indirect
+	github.com/davecgh/go-spew v1.1.1
 	github.com/fatih/color v1.10.0 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/line/line-bot-sdk-go v7.7.0+incompatible
 	github.com/pelletier/go-toml v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
+	gopkg.in/ini.v1 v1.62.0
 )
--- main.go
+++ main.go
@@ -1,26 +1,84 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
+	"encoding/json"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/line/line-bot-sdk-go/linebot" //==========この行を追加==========
+	"gopkg.in/ini.v1"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request)  {
-	t, err := template.ParseFiles("Views/index.html")
-	if err != nil{
-		fmt.Println("error")
+type Webhook struct {
+	Destination string           `json:"destination"`
+	Events      []*linebot.Event `json:"events"`
+}
+
+func LineHandler(w http.ResponseWriter, r *http.Request) {
+
+	cfg, _ := ini.Load("config.ini")
+
+	accessToken := cfg.Section("line").Key("channel_access_token").MustString("")
+	channelSecret := cfg.Section("line").Key("channel_secret").MustString("")
+
+	bot, _ := linebot.New(
+		channelSecret,
+		accessToken,
+	)
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	var webhook Webhook
+
+	if err := json.Unmarshal(body, &webhook); err != nil {
+		log.Print(err)
 	}
-	 if err := t.Execute(w,nil); err != nil{
-	 	fmt.Println("error")
-	 }
+	for _, event := range webhook.Events {
+		switch event.Type {
+			case linebot.EventTypeMessage:
+				switch m := event.Message.(type) {
+				case *linebot.TextMessage:
+					spew.Dump(m)
+					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(m.Text)).Do()
+				}
+		}
+	}
+
+
+	//for _, event := range webhook.Events {
+	//	switch event.Type {
+	//	case linebot.EventTypeMessage: //type: messageのとき
+	//		switch m := event.Message.(type) {
+	//		case *linebot.TextMessage: //type: textのとき
+	//			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(m.Text)).Do(); err != nil {
+	//				log.Print(err)
+	//				return events.APIGatewayProxyResponse{
+	//					StatusCode: http.StatusInternalServerError,
+	//					Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+	//				}, nil
+	//			}
+	//		}
+	//	}
+	//}
+	//return events.APIGatewayProxyResponse{
+	//	StatusCode: 200,
+	//}, nil
 
 }
 
-func main()  {
+//func IndexHandler(w http.ResponseWriter, r *http.Request)  {
+//	t, err := template.ParseFiles("Views/index.html")
+//	if err != nil{
+//		fmt.Println("error")
+//	}
+//	 if err := t.Execute(w,nil); err != nil{
+//	 	fmt.Println("error")
+//	 }
+//
+//}
 
-	http.HandleFunc("/", IndexHandler)
+func main() {
+	http.HandleFunc("/", LineHandler)
 	log.Fatalln(http.ListenAndServe(":8070", nil))
-
 }
